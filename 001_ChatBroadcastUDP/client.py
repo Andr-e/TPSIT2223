0b7c508b38d31d7e33a8c4be1ff279833cf8a307
andryx <armandoandrea27@gmail.com>
2022-09-18 09:50:44 +0200

    Modifica della precedente chat UDP BroadCast

--- 001_ChatBroadcastUDP/client.py
+++ 001_ChatBroadcastUDP/client.py
@@ -2,10 +2,17 @@ from socket import AF_INET, SOCK_DGRAM, socket
 
 def chatClient():
     with socket(AF_INET, SOCK_DGRAM) as s:
-        msg = "hello world"
-        msg = msg.encode('utf8')
+        res = input("Inserisci l'ip di broadCast: ")
+        while True:
+            msg = input("Che messaggio vuoi mandare ?: ")
+            #Codifica il messaggio in bytes
+            msg = msg.encode('utf8')
+            #Manda il messaggio al broadcast sulla porta 5000
+            s.sendto(msg, (f"{res}", int(5000)))
+          
 
-        s.sendto(msg, ("192.168.98.255", 5000))
-
+       
 if __name__ == "__main__":
     chatClient()
+
+
--- 001_ChatBroadcastUDP/server.py
+++ 001_ChatBroadcastUDP/server.py
@@ -1,26 +1,51 @@
 from socket import AF_INET, SO_BROADCAST, SOCK_DGRAM, socket
 from ssl import SOL_SOCKET
-
+#Quanti bit il nostro testo ha (massimi)
+#bytes é un oggetto usato da python per gestire le str
+#Se voglio trasferire una stinga la devo sempre transformare in bytes
 BUFFER_SIZE = 1024
-
-mystr = "ciao" # str
-# bytes
 
 HOST = "0.0.0.0"
 PORT = 5000
-# possibilità
-# localhost 127.0.0.1 
+#Possibiltà
+#LocalHost 127.0.0.1
+#SOCK_DGRAM si intente il protocolo UDP
+
+
+def scriviSuFile(msg):
+    file = open("indirizzi.csv", "a")
+    file.write(msg)
+    file.write("\n")
+    
+    
+    
 
 
 def chatServer():
     with socket(AF_INET, SOCK_DGRAM) as s:
+        #Devo far capire che sto ascoltando dentro il bind metto dentro l'indirizzo da cui ascolo ed il secondo è la porta
         s.bind((HOST, PORT))
-        s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-
+        s.setsockopt(SOL_SOCKET, SO_BROADCAST,1)
+        print("server in ascolto")
+        #Dichiaro una variabile msg per ricevere dati
         while True:
             msg = s.recvfrom(BUFFER_SIZE)
-            msg = msg[0].decode('utf8')
+            #Decodifico da bytes a str
+            msg = msg[0].decode("utf8")
             print(msg)
+            scriviSuFile(msg)
+            
+           
+       
+      
 
+       
+
+
+ 
+
+
+    
+#Questo if lo eseguiamo solo da terminale
 if __name__ == "__main__":
     chatServer()
